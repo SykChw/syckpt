8bb873538a316cb06bc012bb0cf85e7d0a1e3775
SykChw <sayakchowdhury384@gmail.com>
2026-03-23 21:14:35 +0530

    v0.0.5

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ long_description = (
 
 setup(
     name="syckpt",
-    version="0.0.4",
+    version="0.0.5",
     description="Git-like experiment tracking for deep learning with LSH hashing",
     long_description=long_description,
     long_description_content_type="text/markdown",
